548ce2aef4e8b51e4ecd50e1806c458b9a390b81
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2019-12-11 17:27:55 +0000

    intro to what this proto is for

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Casework responsive design template - exploring ideas',
 
   // Default port that prototype runs on
   port: '3000',
